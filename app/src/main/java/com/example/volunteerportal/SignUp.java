552f5b7c1b46ce004a20813a515c3fa18c9906d2
chinmaychahar <chinmay.cc.06@gmail.com>
2021-03-13 23:35:33 +0530

    Volunteer Portal first draft

---
+++ app/src/main/java/com/example/volunteerportal/SignUp.java
@@ -0,0 +1,48 @@
+package com.example.volunteerportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUp extends AppCompatActivity {
+
+    //variables
+    TextInputLayout regUsername,regEmail,regPhoneNo,regPassword;
+    Button regBtn,regToLoginBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_sign_up);
+
+        //hooks
+        regUsername = findViewById(R.id.reg_username);
+        regEmail = findViewById(R.id.reg_email);
+        regPhoneNo = findViewById(R.id.reg_phoneNo);
+        regPassword = findViewById(R.id.reg_password);
+        regBtn = findViewById(R.id.reg_button);
+        regToLoginBtn = findViewById(R.id.reg_login_btn);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+
+                reference.setValue("Data stored");
+            }
+        });
+
+    }
+}
